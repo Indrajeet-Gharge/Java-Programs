86fed74e03b5ab8ef95bfca21cce087f120825ce
Indrajeet-Gharge <85387813+Indrajeet-Gharge@users.noreply.github.com>
2021-08-09 10:51:12 +0530

    Create string.java

---
+++ String/string.java
@@ -0,0 +1,19 @@
+class string
+{
+	public static void main(String[] args) 
+	{
+		int i;
+		String a = "Welcome";
+		String b = "to";
+		String c = "java";
+		System.out.println(a);
+		i = a.length();
+		System.out.println("Length: "+i);
+		i = b.length();
+		System.out.println(b);
+		System.out.println("Length: "+i);
+        i = c.length();
+		System.out.println(c);
+		System.out.println("Length: "+i);
+	}
+}
